66400a43092c54b253f55b2fea29cadef38c9b88
Linus Bohwalli <linus.bohwalli@gmail.com>
2017-01-30 21:54:52 +0100

    and removing file

--- go-client/client_test.go
+++ go-client/client_test.go
@@ -3,25 +3,4 @@ package go_client_test
 import "testing"
 
 func TestOpen(t *testing.T) {
-	//create mocks?
 }
-
-/*
-func TestGetFeaturesForProperties(t *testing.T) {
-	c := NewClient()
-	c.cc = grpc.Dial
-	c.fs = api.NewFeatureToggleServiceClient(cc)
-
-}
-
-func (c *Client) GetFeaturesForProperties(req *api.GetFeaturesByPropertiesRequest) (*api.GetFeaturesByPropertiesResponse, error) {
-
-	resp, err := c.fs.GetFeaturesForProperties(context.Background(), req)
-	if err != nil {
-		grpclog.Printf("Get features for properties failed: %v", err)
-		return nil, err
-	}
-
-	return resp, nil
-}
-*/
